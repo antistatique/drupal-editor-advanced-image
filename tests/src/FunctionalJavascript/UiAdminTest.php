baffbd6b77bcaeaca4a73e3dc2826b8ac9cdcb5c
Kevin Wenger <wenger.kev@gmail.com>
2017-09-28 09:20:55 +0200

    add tests for Editor Admin

---
+++ tests/src/FunctionalJavascript/UiAdminTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\Tests\editor_advanced_image\FunctionalJavascript;
+
+use Drupal\filter\Entity\FilterFormat;
+
+/**
+ * Tests ckeditor formats admin forms.
+ *
+ * @group editor_advanced_image
+ * @group editor_advanced_image_ui_admin
+ */
+class UiAdminTest extends UiTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'filter',
+    'editor',
+    'ckeditor',
+    'editor_advanced_image',
+  ];
+
+  /**
+   * We use the minimal profile because we want to test local action links.
+   *
+   * @var string
+   */
+  protected $profile = 'minimal';
+
+  /**
+   * A user with the 'administer filters' permission.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create text format.
+    $full_html_format = FilterFormat::create([
+      'format'  => 'full_html',
+      'name'    => 'Full HTML',
+      'weight'  => 0,
+      'filters' => [],
+    ]);
+    $full_html_format->save();
+
+    // Create a user for tests.
+    $this->adminUser = $this->drupalCreateUser(['administer filters']);
+  }
+
+  /**
+   * Tests a CKEditor editor and visibilit of Editor Advanced Image config.
+   */
+  public function testAdminForm() {
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('admin/config/content/formats/manage/full_html');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Select the "CKEditor" editor and click the "Save configuration" button.
+    $this->fillField('Text editor', 'ckeditor');
+
+    // Wait on CKEditor Ajax call to load plugins forms.
+    $this->assertSession()->assertWaitOnAjaxRequest();
+
+    // Check the Editor Advanced Image tab is visible.
+    $this->assertElementPresent('.vertical-tabs');
+
+    // Ensure the default class is initialized to the expected default value.
+    $this->assertFieldByName('editor[settings][plugins][editoradvancedimage][default_class]', '');
+  }
+
+  /**
+   * Tests a CKEditor editor & storage of default class field.
+   */
+  public function testDefaultClass() {
+    $this->testAdminForm();
+
+    $this->fillField('editor[settings][plugins][editoradvancedimage][default_class]', 'my-class');
+    $this->assertFieldByName('editor[settings][plugins][editoradvancedimage][default_class]', 'my-class');
+
+    // Submit the new value.
+    $this->pressButton('edit-actions-submit');
+
+    // Return on the editor configuration.
+    $this->drupalGet('admin/config/content/formats/manage/full_html');
+
+    // Ensure the previously filled data has been stored.
+    $this->assertFieldByName('editor[settings][plugins][editoradvancedimage][default_class]', 'my-class');
+  }
+
+}
---
+++ tests/src/FunctionalJavascript/UiTestBase.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\Tests\editor_advanced_image\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+use Behat\Mink\Exception\ElementNotFoundException;
+
+/**
+ * Has some additional helper methods to make test code more readable.
+ */
+abstract class UiTestBase extends JavascriptTestBase {
+
+  /**
+   * Finds field (input, textarea, select) with specified locator.
+   *
+   * @param string $locator
+   *   Input id, name or label.
+   *
+   * @return \Behat\Mink\Element\NodeElement|null
+   *   The input field element.
+   */
+  public function findField($locator) {
+    return $this->getSession()->getPage()->findField($locator);
+  }
+
+  /**
+   * Finds button with specified locator.
+   *
+   * @param string $locator
+   *   Button id, value or alt.
+   *
+   * @return \Behat\Mink\Element\NodeElement|null
+   *   The button node element.
+   */
+  public function findButton($locator) {
+    return $this->getSession()->getPage()->findButton($locator);
+  }
+
+  /**
+   * Presses button with specified locator.
+   *
+   * @param string $locator
+   *   Button id, value or alt.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   */
+  public function pressButton($locator) {
+    $this->getSession()->getPage()->pressButton($locator);
+  }
+
+  /**
+   * Click the element with specified selector.
+   *
+   * @param string $selector_type
+   *   The element selector type (CSS, XPath).
+   * @param string|array $selector
+   *   The element selector. Note: the first found element is used.
+   *
+   * @return \Behat\Mink\Element\NodeElement|null
+   *   The node element.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   */
+  public function clickOnElement($selector_type, $selector) {
+    $element = $this->getSession()->getPage()->find($selector_type, $selector);
+
+    if (!$element) {
+      if (is_array($selector)) {
+        $selector = implode(' ', $selector);
+      }
+      throw new ElementNotFoundException($this->getSession()->getDriver(), 'element', $selector_type, $selector);
+    }
+
+    $element->click();
+    return $element;
+  }
+
+  /**
+   * Fills in field (input, textarea, select) with specified locator.
+   *
+   * @param string $locator
+   *   Input id, name or label.
+   * @param string $value
+   *   Value.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   *
+   * @see \Behat\Mink\Element\NodeElement::setValue
+   */
+  public function fillField($locator, $value) {
+    $this->getSession()->getPage()->fillField($locator, $value);
+  }
+
+  /**
+   * Make a screenshot and store it in the public://screenshots uri.
+   */
+  public function debugScreenshot() {
+    $screenshot = \Drupal::root() . '/sites/default/files/simpletest/screenshot-' . time() . '.jpg';
+    $this->createScreenshot($screenshot);
+    echo "\n" . $screenshot . "\n";
+  }
+
+}
